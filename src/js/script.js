9af41cd86d70a629f7fca85cb7c24d1fcd864638
jj.attaq <jj.attaq@tutanota.com>
2022-04-11 21:24:45 +0200

    playRound() experiment -- is inconclusive

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 function computerPlay() { //  returns rock, paper, or scissors randomly
     // define R, P, and S
-    const rock = () => console.log('rock');
-    const paper = () => console.log('paper');
-    const scissors = () => console.log('scissors');
+    const rock = () => console.log(`rock`);
+    const paper = () => console.log(`paper`);
+    const scissors = () => console.log(`scissors`);
     // random numbers from 1 to 3
     const random = Math.floor((Math.random() * 3) + 1);
     // return R, P, or S
@@ -12,12 +12,17 @@ function computerPlay() { //  returns rock, paper, or scissors randomly
 }
 function playRound(playerSelection, computerSelection) {
     // define draw
-    if (playerSelection === computerSelection) {
-        console.log('draw!')
-    }
-    // define win
-
-    // define loss
+    // if (playerSelection === computerSelection) {
+    //     console.log('draw!')
+    // }
+    // define win and lose
+    const win = (playerSelection === 1 && computerSelection === 3) || (playerSelection === 2 && computerSelection === 1) || (playerSelection === 3 && computerSelection === 2);
+    const lose = (playerSelection === 1 && computerSelection === 2) || (playerSelection === 2 && computerSelection === 3) || (playerSelection === 3 && computerSelection === 1); 
+    const draw = playerSelection === computerSelection;
+    
+    if (win) {console.log(`you win!`);}
+    if (lose) {console.log(`you lose!`);}
+    if (draw) {console.log(`it's a draw!`);}
 }
-const playerSelection = 'rock';
+const playerSelection = 1;
 const computerSelection = computerPlay();
