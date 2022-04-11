192af1a44e72d2e853b88b565fcbd000d379a126
jj.attaq <jj.attaq@tutanota.com>
2022-04-11 22:40:43 +0200

    further experimentation, inconclusive -- will branch and continue

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,31 @@
-function computerPlay() { //  returns rock, paper, or scissors randomly
-    // define R, P, and S
-    const rock = () => console.log(`rock`);
-    const paper = () => console.log(`paper`);
-    const scissors = () => console.log(`scissors`);
-    // random numbers from 1 to 3
-    const random = Math.floor((Math.random() * 3) + 1);
-    // return R, P, or S
-    if (random === 1) {rock();} 
-    if (random === 2) {paper();} 
-    if (random === 3) {scissors();}
-}
+
 function playRound(playerSelection, computerSelection) {
     // define draw
     // if (playerSelection === computerSelection) {
     //     console.log('draw!')
     // }
     // define win and lose
+    function computerPlay() { //  returns rock, paper, or scissors randomly
+        // define R, P, and S
+            // const rock = () => `rock` && console.log(`rock`);
+            // const paper = () => `paper` && console.log(`paper`);
+            // const scissors = () => `scissors` && console.log(`scissors`);
+            function rock() {
+                return 1 && console.log(`rock`);
+            }
+            function paper() {
+                return 2 && console.log(`paper`);
+            }
+            function scissors() {
+                return 3 && console.log(`scissors`);
+            }
+        // random numbers from 1 to 3
+            const random = Math.floor((Math.random() * 3) + 1);
+        // return R, P, or S
+            if (random === 1) {rock();} 
+            if (random === 2) {paper();} 
+            if (random === 3) {scissors();}
+        }
     const win = (playerSelection === 1 && computerSelection === 3) || (playerSelection === 2 && computerSelection === 1) || (playerSelection === 3 && computerSelection === 2);
     const lose = (playerSelection === 1 && computerSelection === 2) || (playerSelection === 2 && computerSelection === 3) || (playerSelection === 3 && computerSelection === 1); 
     const draw = playerSelection === computerSelection;
@@ -26,3 +36,4 @@ function playRound(playerSelection, computerSelection) {
 }
 const playerSelection = 1;
 const computerSelection = computerPlay();
+const play = playRound();
