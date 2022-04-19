8e1077d1ad29352f2547a979b475db3d92c988e1
jj.attaq <jj.attaq@tutanota.com>
2022-04-19 18:55:43 +0200

    finished OP assignment

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,76 @@
-function computerPlay() { // computer chooses between R, P, or S from array randomly
-  const random = Math.floor(Math.random() * choice.length);
-  console.log(random, choice[random]);
-  return choice[random];
-}
+let tally = 0;
+
 function playRound(playerSelection, computerSelection) { // play one round of R, P, S
-  const win = (playerSelection === `rock` && computerSelection === `scissors`) || 
-    (playerSelection === `paper` && computerSelection === `rock`) || 
-		(playerSelection === `scissors` && computerSelection === `paper`);
-  const lose = (playerSelection === `rock` && computerSelection === `paper`) || 
-    (playerSelection === `paper` && computerSelection === `scissors`) || 
-    (playerSelection === `scissors` && computerSelection === `rock`); 
-  const draw = playerSelection === computerSelection;
-	function result(gameState) {
-		if (win) {alert(`You win! ${playerSelection} beats ${computerSelection}!`);gameState = `win`;return gameState;}
-		if (lose) {alert(`You lose! ${computerSelection} beats ${playerSelection}!`);gameState = `lose`;return gameState;}
-		if (draw) {alert(`It's a draw!`);gameState = `draw`;return gameState;} 
+	const choice = [`rock`, `paper`, `scissors`];
+	computerSelection = computerPlay();
+	playerSelection = prompt(`please write in 'rock', 'paper' or 'scissors'`, ).toLowerCase();
+
+	function computerPlay() { // computer chooses between R, P, or S from array randomly
+		const random = Math.floor(Math.random() * choice.length);
+		console.log(random, choice[random]);
+		return choice[random];
 	}
-	return result();
+
+  const win = (playerSelection === `rock` && computerSelection === `scissors`) ||
+    (playerSelection === `paper` && computerSelection === `rock`) ||
+		(playerSelection === `scissors` && computerSelection === `paper`);
+  const lose = (playerSelection === `rock` && computerSelection === `paper`) ||
+    (playerSelection === `paper` && computerSelection === `scissors`) ||
+    (playerSelection === `scissors` && computerSelection === `rock`);
+  const draw = playerSelection === computerSelection;
+
+	if (win) {alert(`You win! ${playerSelection} beats ${computerSelection}!`);console.log(`win`);tally++;return `win`;}
+	if (lose) {alert(`You lose! ${computerSelection} beats ${playerSelection}!`);console.log(`lose`);tally--;return`lose`;}
+	if (draw) {alert(`It's a draw!`);console.log(`draw`);tally + 0;return `draw`;}
 }
-const choice = [`rock`, `paper`, `scissors`];
-const playerSelection = prompt(`please write in 'rock', 'paper' or 'scissors'`, ).toLowerCase();
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+	for (let i = 0; i < 5; i++) {
+		if (true) {
+			playRound();
+			console.log(tally);
+		}
+		if (tally === 3 || tally === -3)
+		break;
+	}
+	if (tally > 0) {
+		alert(`YOU WON THE GAME!`);
+	}
+	if (tally < 0) {
+		alert(`YOU LOST THE GAME!`);
+	}
+	if (tally === 0) {
+		alert(`BOOOORING! It's a draw.`);
+	}
+}
+game();
+
+// function gameResult() {
+// 	let tally = 0;
+// 	if (playRound() === `win`) {
+// 		tally++;
+// 	}
+// 	if (playRound() === `lose`) {
+// 		tally--;
+// 	}
+// 	if (playRound() === `draw`) {
+// 		tally + 0;
+// 	}
+// 	return tally;
+// }
+
+
+// function gameResult(tally) {
+// 	switch (tally) {
+// 		case playRound() === `win`:
+// 			tally++;
+// 			break;
+// 		case playRound() === `lose`:
+// 			tally--;
+// 			break;
+// 		case playRound() === `draw`:
+// 			tally === + 0;
+// 			break;
+// 	}
+
+// }
+// gameResult();
