d98780bf54d9a45acc1959e4c97d54e1b82a2e3b
jj.attaq <jj.attaq@tutanota.com>
2022-05-18 16:30:42 +0200

    assignment complete; could use some cleaning up

--- src/js/script.js
+++ src/js/script.js
@@ -1,78 +1,132 @@
 const body = document.body;
-const r = `rock`;
-const p = `paper`;
-const s = `scissors`;
-const compOptions = [r, p, s];
-
+const r = `rock`,
+			p = `paper`,
+			s = `scissors`;
 const buttons = document.querySelectorAll(`.btn`);
+let tally = 0;
+let numberOfRounds = 1;
+
 buttons.forEach((button) => {
 	button.addEventListener(`click`, () => {
-		console.log(`player is ${button.id}`)
 		pSelection = button.id;
 		playRound();
 	});
 });
 
-// --------------------------------------------
-let tally = 0;
-
-function playRound(pSel, compSel) { 
+function playRound(pSel, compSel) {
+	const container = document.querySelector(`#displayResults`),
+				result = document.createElement(`p`);
 	compSel = computerPlay();
 	pSel = pSelection;
 
 	function computerPlay() { 
-		const random = Math.floor(Math.random() * compOptions.length);
-		console.log(compOptions[random]);
+		const compOptions = [r, p, s],
+					random = Math.floor(Math.random() * compOptions.length);
 		return compOptions[random];
 	}
-	
-  const win = (pSel === r && compSel === s) || (pSel === p && compSel === r) || (pSel === s && compSel === p);
-	const draw = pSel === compSel;
-  const lose = !win && !draw;
-	
-	if (win) {alert(`You win! ${pSel} beats ${compSel}!`);tally++;return `win`;}
-	if (lose) {alert(`You lose! ${compSel} beats ${pSel}!`);tally--;return `loss`;}
-	if (draw) {alert(`It's a draw!`);tally + 0;return `draw`;}
-}
 
-function game() {
-	for (let i = 1; i <= 5; i++) {
-		if (tally === 3 || tally === -3) {
-			break;
+	const win = (pSel === r && compSel === s) || (pSel === p && compSel === r) || (pSel === s && compSel === p),
+				draw = pSel === compSel,
+				lose = !win && !draw,	
+				winRoundText = () => result.textContent = `You win the round! ${pSel} beats ${compSel}! Your score is ${tally}`,
+				loseRoundText = () => result.textContent = `You lost the round! ${compSel} beats ${pSel}! Your score is ${tally}`, 
+				roundDrawText = () => result.textContent = `The round is a draw! Your score is ${tally}`,
+				victory = () => result.textContent = `Your score is ${tally}. YOU WON THE GAME!`,
+				defeat = () => result.textContent =`Your score is ${tally}. YOU LOST THE GAME!`,
+				undecided = () => result.textContent = `Your score is ${tally}. BOOOORING! It's a draw.`;			
+
+	const winRound = function() {
+		if (win) {
+			tally++;
+			numberOfRounds++; 
+			winRoundText();
+			container.appendChild(result);
 		} else {
-			console.log(`Round ${i} is a ${playRound()}. Your current score is ${tally} points.`);
+			return;
 		}
 	}
-	if (tally > 0) {
-		alert(`YOU WON THE GAME!`);
+	const loseRound = function() {
+		if (lose) {
+			tally--;
+			numberOfRounds++;
+			loseRoundText();
+			container.appendChild(result);
+		} else {
+			return;
+		}
 	}
-	if (tally < 0) {
-		alert(`YOU LOST THE GAME!`);
+	const roundDraw = function() {
+		if (draw) {
+			numberOfRounds++;
+			roundDrawText();
+			container.appendChild(result);
+		}  else {
+			return;
+		}
 	}
-	if (tally === 0) {
-		alert(`BOOOORING! It's a draw.`);
+	const winThree = function() {
+		if (tally === 3) {
+			winRound();
+			victory();
+		} else {
+			return;
+		}
+	}
+	const loseThree = function() {
+		if (tally === -3) {
+			loseRound();
+			defeat();
+		} else {
+			return;
+		} 
+	}
+	
+	fullGame();
+	function fullGame() {
+		let gameResult = false;
+		winThree();
+		loseThree();			
+		determineIfGameEnd();
+		
+		function determineIfGameEnd() {
+			if (numberOfRounds === 5) {
+				winRound();
+				loseRound();
+				roundDraw();
+				if (tally > 0) victory() && (gameResult = true);
+				if (tally < 0) defeat() && (gameResult = true);
+				if (tally === 0) undecided() && (gameResult = true); 
+				container.appendChild(result); 
+				createReplayButton();
+			}	else {
+					winRound();
+					loseRound();
+					roundDraw();
+			}
+		}
+		function createReplayButton() {
+			replayButton = document.createElement('button'); 
+			replayButton.textContent = `Click to play again.`;
+			replayButton.setAttribute(`id`, `replay`);
+			replayButton.addEventListener(`click`, () => {
+				endGame();
+			});
+			container.append(replayButton);
+		}
+		
+		function endGame() { 	
+			if(gameResult === true) {
+				tally = 0;
+				numberOfRounds = 1;
+				clearBox();
+				function clearBox() {
+					while (container.firstChild) {
+						container.removeChild(container.firstChild);
+					}
+				}
+			} else {
+				return;
+			}
+		}
 	}
 }
-
-// game();
-
-
-
-//------------------
-
-// const chooseR = document.getElementById(`rock`);
-// const chooseP = document.getElementById(`paper`);
-// const chooseS = document.getElementById(`scissors`);
-
-// const pR = chooseR.addEventListener('click', (e) => {
-// 	return r;
-// 	console.log(r);
-// });
-// const pP = chooseP.addEventListener('click', () => {
-// 	// return p;
-// 	console.log(p);
-// });
-// const pS = chooseS.addEventListener('click', () => {
-// 	// return s;
-// 	console.log(s);
-// });
